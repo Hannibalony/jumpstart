d05b3eb2841add70e86487110663e863d0cf880e
Hannibalony <justinecantado@gmail.com>
2017-05-26 01:56:17 +0000

    updates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,41 @@
 class ArticlesController < ApplicationController
-  
+  include ArticlesHelper
+
   def index
-    @articles = Article.all
+    @article = Article.all
   end
   
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
   
+  def new 
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+    flash.notice = "#{@article.title} Has been created!"
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash.notice = "#{@article.title} Has been deleted!"
+  end
+  
+  def edit 
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+    flash.notice = "#{@article.title} Has been updated!"
+  end
 end
 
